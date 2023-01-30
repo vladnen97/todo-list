f5a564daffcbe75df9fbb5bf07262bfac5e223c4
vladnen97 <darkdescent1991@gmail.com>
2023-01-30 21:56:06 +0300

    first lesson: introducing to components(props) and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./components/Todolist";
 
 function App() {
+
+    const title1 = 'What to learn-1';
+    const title2 = 'What to learn-222';
+
+    const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: false },
+        { id: 2, title: "JS", isDone: false },
+        { id: 3, title: "ReactJS", isDone: false },
+    ]
+    const tasks2 = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am Happy", isDone: true },
+        { id: 3, title: "Yo", isDone: true }
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title={title1} tasks={tasks1}/>
+            <Todolist title={title2} tasks={tasks2}/>
         </div>
     );
 }
---
+++ src/components/Todolist.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+type TodolistPropsType = {
+    title?: string,
+    tasks: Array<TaskType>
+}
+
+type TaskType = {
+    id: number,
+    title: string,
+    isDone: boolean
+}
+
+export const Todolist = ({title, tasks}: TodolistPropsType) => {
+
+    let list = tasks.map(elem => <li key={elem.id}><input type="checkbox" checked={elem.isDone}/> <span>{elem.title}</span></li>)
+
+    return (
+            <div>
+                <h3>{title}</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    {list}
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+    );
+};
+
