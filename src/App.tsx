be1d1c6fadc89554a078762f5b2796be0089688b
vladnen97 <darkdescent1991@gmail.com>
2023-02-01 23:08:08 +0300

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import {Todolist} from "./components/Todolist";
 
 function App() {
 
-    const title1 = 'What to learn-1';
-    const title2 = 'What to learn-222';
+    const title1 = 'What to learn';
+    const title2 = 'What to watch';
 
     const tasks1 = [
-        { id: 1, title: "HTML&CSS", isDone: false },
-        { id: 2, title: "JS", isDone: false },
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
     ]
     const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: true },
-        { id: 3, title: "Yo", isDone: true }
+        { id: 1, title: "FMJ", isDone: true },
+        { id: 2, title: "Dune", isDone: true },
+        { id: 3, title: "Mad Max", isDone: true }
     ]
 
     return (
