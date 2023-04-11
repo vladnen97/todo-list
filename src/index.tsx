0ea3b975052b8b0fd517566411633564e5646e2a
vladnen97 <darkdescent1991@gmail.com>
2023-04-11 06:43:44 +0300

    connect app that using useReducer instead old app with useState

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './index.css';
 import {App} from './App';
 import { createRoot } from 'react-dom/client';
+import {AppWithUseReducer} from './AppWithUseReducer';
 
 const container  = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
-root.render(<App />);
+root.render(<AppWithUseReducer />);
 
 
 
