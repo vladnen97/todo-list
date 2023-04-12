39dfd2dc38dedcb6e7f8cbdf0a394c1c6b59e091
vladnen97 <darkdescent1991@gmail.com>
2023-04-12 22:32:57 +0300

    create task component

---
+++ src/Task.tsx
@@ -0,0 +1,32 @@
+import React, {ChangeEvent, memo, useCallback} from 'react';
+import {Checkbox, IconButton} from '@mui/material';
+import {DisabledByDefaultOutlined} from '@mui/icons-material';
+import {EditableSpan} from './EditableSpan';
+import {TaskType} from './AppWithRedux';
+
+type TaskPropsType = {
+    task: TaskType
+    removeTask: (taskId: string) => void
+    changeStatus: (taskId: string, isDone: boolean) => void
+    changeTaskTitle: (taskId: string, newTitle: string) => void
+}
+
+export const Task = memo((props: TaskPropsType) => {
+
+    const onClickHandler = () => props.removeTask(props.task.id)
+    const onChangeStatusHandler = (e: ChangeEvent<HTMLInputElement>) => props.changeStatus(props.task.id, e.currentTarget.checked)
+    const onChangeTaskTitleHandler = useCallback((value: string) => {props.changeTaskTitle(props.task.id, value)}, [props.changeTaskTitle, props.task.id])
+
+    return (
+        <li className={props.task.isDone ? 'is-done' : ''} >
+            <IconButton  aria-label="remove" size="small" onClick={onClickHandler}>
+                <DisabledByDefaultOutlined fontSize="medium"/>
+            </IconButton>
+            <Checkbox checked={props.task.isDone}
+                      onChange={onChangeStatusHandler}
+                      size={'medium'}/>
+            <EditableSpan title={props.task.title} onChange={onChangeTaskTitleHandler}/>
+        </li>
+    );
+})
+
