154f8bfac42ed2da422a95b3a4c034dd4c94aeed
vladnen97 <darkdescent1991@gmail.com>
2023-04-19 20:28:19 +0300

    refactoring

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -1,18 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import {AppWithRedux} from '../AppWithRedux';
-import {Provider} from 'react-redux';
-import {store} from '../store/store';
+import {ReduxStoreProviderDecorator} from '../store/ReduxStoreProviderDecorator';
 
 
 const meta: Meta<typeof AppWithRedux> = {
     title: 'Todolist/App',
     component: AppWithRedux,
     tags: ['autodocs'],
+    decorators: [ReduxStoreProviderDecorator]
 };
 
 export default meta;
 type Story = StoryObj<typeof AppWithRedux>;
 
-export const AppStory: Story = {
-    render: () =><Provider store={store}><AppWithRedux/></Provider>
-}
+export const AppStory: Story = {}
