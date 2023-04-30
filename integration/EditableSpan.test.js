0fca8c484574bd90a44ea371d01d5b6700e45f9a
vladnen97 <darkdescent1991@gmail.com>
2023-04-30 20:11:50 +0300

    snapshot test for editable span

---
+++ integration/EditableSpan.test.js
@@ -0,0 +1,17 @@
+
+
+describe('editableSpan', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:6006/iframe.html?args=&id=todolist-editablespan--editable-span-story&viewMode=story',{waitUntil: "networkidle2"})
+
+        // await page.waitForTimeout(2000)  {waitUntil: "networkidle2"}
+
+        const image = await page.screenshot()
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot()
+    })
+})
+
+//yarn jest:integration --updateSnapshot
