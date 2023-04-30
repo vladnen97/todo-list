244cd3fbcbed31aa758b6f945a2ff819669cd3be
vladnen97 <darkdescent1991@gmail.com>
2023-04-30 20:12:25 +0300

    remove deprecated timeout and add option

--- integration/addItemForm.test.js
+++ integration/addItemForm.test.js
@@ -3,9 +3,7 @@
 describe('addItemForm', () => {
     it('base example, visually looks correct', async () => {
         // APIs from jest-puppeteer
-        await page.goto('http://localhost:6006/iframe.html?args=&id=todolist-additemform--add-item-form-story&viewMode=story')
-
-        await page.waitForTimeout(2000)
+        await page.goto('http://localhost:6006/iframe.html?args=&id=todolist-additemform--add-item-form-story&viewMode=story',{waitUntil: "networkidle2"})
 
         const image = await page.screenshot()
 
