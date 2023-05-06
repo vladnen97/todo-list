72559ea01506e7f46e425ad16a7e62cbbec0dd3f
vladnen97 <darkdescent1991@gmail.com>
2023-05-06 23:07:20 +0300

    stories with api

---
+++ src/stories/tasks-api.stories.tsx
@@ -0,0 +1,136 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {useState} from 'react';
+import {tasksAPI} from '../api/tasks-api';
+
+
+const meta: Meta = {
+    title: 'API/Tasks',
+};
+
+export default meta
+type Story = StoryObj
+type TaskModelType = {
+    title: string
+    description: string
+    status: number
+    priority: number
+    startDate: string
+    deadline: string
+}
+
+const GetC = () => {
+    const [state, setState] = useState<any>([])
+    const [todolistId, setTodolistId] = useState('dd6682f6-1324-41af-ad1e-2ceb60f4ae75')
+
+    const onClickHandler = () => {
+        tasksAPI.getTasks(todolistId).then(res => setState(res.data.items.map(el => `${el.title} : ${el.id} : ${el.status}`)))
+        setTodolistId('')
+    }
+
+    return (
+        <>
+            <ul>
+                {state.length !== 0
+                    ? state.map((el: any) => <li key={el}>{el}</li>)
+                    : 'no tasks'
+                }
+            </ul>
+            <input value={todolistId} placeholder={'enter todolist id '}
+                   onChange={e => setTodolistId(e.currentTarget.value)}/>
+            <button onClick={onClickHandler}>get tasks</button>
+        </>
+    )
+}
+export const GetTasks: Story = {
+    render: () => <GetC/>
+}
+
+const PostC = () => {
+    const [state, setState] = useState<any>(null)
+    const [todolistId, setTodolistId] = useState<string>('dd6682f6-1324-41af-ad1e-2ceb60f4ae75')
+    const [title, setTitle] = useState<string>('')
+
+    const onClickHandler = () => {
+        tasksAPI.createTask(todolistId, title).then(res => {
+            setState(res.data)
+        })
+        setTitle('')
+    }
+
+    return (
+        <>
+            <input placeholder={'enter todolist id'} value={todolistId}
+                   onChange={e => setTodolistId(e.currentTarget.value)} required/>
+            <input placeholder={'enter task title'} value={title} onChange={e => setTitle(e.currentTarget.value)}
+                   required/>
+            <button onClick={onClickHandler}>add task</button>
+            <h2>------------</h2>
+            <div>{JSON.stringify(state)}</div>
+        </>
+    )
+}
+export const CreateTask: Story = {
+    render: () => <PostC/>
+}
+
+const DeleteC = () => {
+    const [state, setState] = useState<any>(null)
+    const [todoId, setTodoId] = useState('dd6682f6-1324-41af-ad1e-2ceb60f4ae75')
+    const [taskId, setTaskId] = useState('')
+
+    const onClickHandler = () => {
+        tasksAPI.deleteTask(todoId, taskId).then(res => setState(res.data))
+        setTaskId('')
+    }
+
+    return (
+        <>
+            <input value={todoId} onChange={e => setTodoId(e.currentTarget.value)} placeholder={'enter todolist id '}/>
+            <input value={taskId} onChange={e => setTaskId(e.currentTarget.value)} placeholder={'enter task id '}/>
+            <button onClick={onClickHandler}>delete task</button>
+            <h3>-----------</h3>
+            <div>{JSON.stringify(state)}</div>
+        </>
+    )
+}
+export const DeleteTask: Story = {
+    render: () => <DeleteC/>
+}
+
+const UpdateC = () => {
+    const [todoId, setTodoId] = useState('dd6682f6-1324-41af-ad1e-2ceb60f4ae75')
+    const [taskId, setTaskId] = useState('4a225ad0-8f7b-4277-acd8-0fd616be703d')
+    const [title, setTitle] = useState('')
+    const [state, setState] = useState<any>(null)
+
+    const taskModel: TaskModelType = {
+        title: title,
+        deadline: '',
+        status: 0,
+        description: '',
+        priority: 1,
+        startDate: ''
+    }
+
+    const onclickHandler = () => {
+        tasksAPI.updateTask(todoId, taskId, taskModel).then(res => setState(res.data))
+    }
+
+    return (
+        <>
+            <div>
+                <input value={todoId} onChange={e => setTodoId(e.currentTarget.value)}
+                       placeholder={'enter todolist id'}/>
+                <input value={taskId} onChange={e => setTaskId(e.currentTarget.value)}
+                       placeholder={'enter task id'}/>
+            </div>
+            <input value={title} onChange={e => setTitle(e.currentTarget.value)} placeholder={'enter task title'}/>
+            <button onClick={onclickHandler}>update task</button>
+            <h2>----------</h2>
+            <div>{JSON.stringify(state)}</div>
+        </>
+    )
+}
+export const UpdateTask: Story = {
+    render: () => <UpdateC/>
+}
---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,115 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {useEffect, useState} from 'react';
+import {todolistsAPI} from '../api/todolists-api';
+
+
+const meta: Meta = {
+    title: 'API/Todolists',
+};
+
+export default meta
+type Story = StoryObj
+
+
+const GetC = () => {
+    const [state, setState] = useState<any>([])
+    useEffect(() => {
+        todolistsAPI.getTodolists().then(res => setState(res.data))
+    }, [])
+
+    return <ul>{state.length !== 0
+        ? state.map((el: any) => <li key={el.id}>{el.title + '  :  ' + el.id}</li>)
+        : 'no todolists'}</ul>
+}
+export const GetTodolists: Story = {
+    render: () => <GetC/>
+}
+
+const PostC = () => {
+    const [state, setState] = useState<any>(null)
+    const [value, setValue] = useState<string>('')
+
+    const onClickHandler = () => {
+        todolistsAPI.createTodolist(value).then(res => {
+            setState(res.data)
+            setValue('')
+        })
+    }
+
+    return (
+        <>
+            <div>{JSON.stringify(state)}</div>
+            <input placeholder={'enter todolist title'} value={value} onChange={(e) => setValue(e.currentTarget.value)}/>
+            <button onClick={onClickHandler}>add todolist</button>
+        </>
+    )
+}
+export const CreateTodolist: Story = {
+    render: () => <PostC/>
+}
+
+const DeleteC = () => {
+    const [todos, setTodos] = useState<any>([])
+    const [state, setState] = useState<any>(null)
+    const [value, setValue] = useState<string>('')
+    useEffect(() => {
+        todolistsAPI.getTodolists().then(res => setTodos(res.data.reduce((acc: Array<string>, el) => [...acc, el.id],[])))
+    }, [state])
+
+    const onClickHandler = () => {
+        todolistsAPI.deleteTodolist(value).then(res => {
+            setState(res.data)
+            setValue('')
+        })
+    }
+
+    return (
+        <>
+            <h3>available todolists id</h3>
+            <ul>{todos.map((el: any) => <li key={el}>{el}</li>)}</ul>
+            <input value={value} onChange={(e) => setValue(e.currentTarget.value)} placeholder={'enter todolist ID'}/>
+            <button onClick={onClickHandler}>delete todolist</button>
+            <div>{JSON.stringify(state)}</div>
+        </>
+    )
+}
+export const DeleteTodolist: Story = {
+    render: () => <DeleteC/>
+}
+
+const UpdateC = () => {
+    const [state, setState] = useState<any>(null)
+    const [todos, setTodos] = useState<Array<string>>([])
+    const [title, setTitle] = useState<string>('')
+    const [id, setId] = useState<string>('')
+
+    useEffect(() => {
+        todolistsAPI.getTodolists().then(res => {
+            setTodos(res.data.reduce((acc: Array<string>, el) => [...acc, el.title + ' : ' + el.id], []))
+        })
+    }, [state])
+
+    const onClickHandler = () => {
+        todolistsAPI.updateTodolist(id, title).then(res => {
+            console.log(res.data)
+            setState(res.data)
+            setTitle('')
+            setId('')
+        })
+    }
+
+    return (
+        <>
+            <h3>todos</h3>
+            <ul>
+                {todos.map(el => <li key={el}>{el}</li>)}
+            </ul>
+            <input placeholder={'enter todolist title'} value={title} onChange={e => setTitle(e.currentTarget.value)}/>
+            <input placeholder={'enter todolist id'} value={id} onChange={e => setId(e.currentTarget.value)}/>
+            <button onClick={onClickHandler}>update title</button>
+        </>
+    )
+}
+export const UpdateTodolistTitle: Story = {
+    render: () => <UpdateC/>
+}
