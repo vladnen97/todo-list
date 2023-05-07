1de1757ede195306133938d727938ae323a4cc31
vladnen97 <darkdescent1991@gmail.com>
2023-05-07 23:22:59 +0300

    reafactoring with new types

--- src/store/ReduxStoreProviderDecorator.tsx
+++ src/store/ReduxStoreProviderDecorator.tsx
@@ -5,6 +5,7 @@ import { v1 } from 'uuid'
 import {RootStateType} from './store';
 import {tasksReducer} from './tasks-reducer';
 import {todolistsReducer} from './todolists-reducer';
+import {TaskStatuses} from '../api/tasks-api';
 
 
 const rootReducer = combineReducers({
@@ -14,17 +15,17 @@ const rootReducer = combineReducers({
 
 const initialGlobalState = {
     todolists: [
-        {id: 'todolistId1', title: 'What to learn', filter: 'all'},
-        {id: 'todolistId2', title: 'What to buy', filter: 'all'}
+        {id: 'todolistId1', title: 'What to learn', filter: 'all', order: 0, addedDate: ''},
+        {id: 'todolistId2', title: 'What to buy', filter: 'all', order: 0, addedDate: ''}
     ],
     tasks: {
         ['todolistId1']: [
-            {id: v1(), title: 'HTML&CSS', isDone: true},
-            {id: v1(), title: 'JS', isDone: true}
+            {id: v1(), title: 'HTML&CSS', status: TaskStatuses.New, todoListId: 'todolistId1', startDate: '', priority: 0, description: '',order: 0, addedDate: '', deadline: ''},
+            {id: v1(), title: 'JS', status: TaskStatuses.Completed, todoListId: 'todolistId1', startDate: '', priority: 0, description: '',order: 0, addedDate: '', deadline: ''}
         ],
         ['todolistId2']: [
-            {id: v1(), title: 'Milk', isDone: true},
-            {id: v1(), title: 'React Book', isDone: true}
+            {id: v1(), title: 'Milk', status: TaskStatuses.New, todoListId: 'todolistId1', startDate: '', priority: 0, description: '',order: 0, addedDate: '', deadline: ''},
+            {id: v1(), title: 'React Book', status: TaskStatuses.Completed, todoListId: 'todolistId1', startDate: '', priority: 0, description: '',order: 0, addedDate: '', deadline: ''}
         ]
     }
 }
