195d29a477a252a3abd171bdf6009c7cd66b67df
vladnen97 <darkdescent1991@gmail.com>
2023-05-20 12:07:28 +0300

    type for dispatch

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
-import {applyMiddleware, combineReducers, legacy_createStore} from 'redux';
+import {applyMiddleware, combineReducers, legacy_createStore, Store} from 'redux';
 import {TodolistsActionsType, todolistsReducer} from './todolists-reducer';
 import {TasksActionsType, tasksReducer} from './tasks-reducer';
-import thunk, {ThunkAction} from 'redux-thunk';
+import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk';
 
 
 const RootState = combineReducers(
@@ -13,9 +13,10 @@ const RootState = combineReducers(
 
 export type RootStateType = ReturnType<typeof RootState>
 type AppActionTypes = TodolistsActionsType | TasksActionsType
+export type AppDispatch = ThunkDispatch<RootStateType, unknown, AppActionTypes>
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootStateType, unknown, AppActionTypes>
 
-export const store = legacy_createStore(RootState, applyMiddleware(thunk))
+export const store: Store<RootStateType, AppActionTypes> = legacy_createStore(RootState, applyMiddleware(thunk))
 
 //@ts-ignore
 window.store = store
