3acc5b4a90e1be5ab0271e67f963a917a5464666
vladnen97 <darkdescent1991@gmail.com>
2023-05-28 21:54:13 +0300

    connect new reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,17 +2,19 @@ import {applyMiddleware, combineReducers, legacy_createStore, Store} from 'redux
 import {TodolistsActionsType, todolistsReducer} from './todolists-reducer';
 import {TasksActionsType, tasksReducer} from './tasks-reducer';
 import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk';
+import {AppActionsType, appReducer} from './app-reducer';
 
 
 const RootState = combineReducers(
     {
         todolists: todolistsReducer,
-        tasks: tasksReducer
+        tasks: tasksReducer,
+        app: appReducer
     }
 )
 
 export type RootStateType = ReturnType<typeof RootState>
-type AppActionTypes = TodolistsActionsType | TasksActionsType
+type AppActionTypes = TodolistsActionsType | TasksActionsType | AppActionsType
 export type AppDispatchType = ThunkDispatch<RootStateType, unknown, AppActionTypes>
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootStateType, unknown, AppActionTypes>
 
