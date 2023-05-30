af48429b905356b4408ede16e2c6a51f400b9edd
vladnen97 <darkdescent1991@gmail.com>
2023-05-30 15:17:22 +0300

    error handlers

---
+++ src/utils/error-utils.ts
@@ -0,0 +1,13 @@
+import {setAppError, setAppStatus} from '../store/app-reducer';
+import {AppDispatchType} from '../store/store';
+import {ResponseType} from '../api';
+
+export const handleServerAppError = <D>(dispatch: AppDispatchType, res: ResponseType<D>) => {
+    dispatch(setAppError(res.messages[0]))
+    dispatch(setAppStatus('failed'))
+}
+
+export const handleServerNetworkError = (dispatch: AppDispatchType, err: any) => {
+    dispatch(setAppError(err.message ? err.message : 'Some error occurred'))
+    dispatch(setAppStatus('failed'))
+}
