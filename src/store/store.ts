e58c64e469c155077bff6302f1689b679c1e6d64
vladnen97 <darkdescent1991@gmail.com>
2023-06-13 22:59:26 +0300

    combine auth reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,18 +3,20 @@ import {TodolistsActionsType, todolistsReducer} from './todolists-reducer';
 import {TasksActionsType, tasksReducer} from './tasks-reducer';
 import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk';
 import {AppActionsType, appReducer} from './app-reducer';
+import {AuthActionsType, authReducer} from './auth-reducer';
 
 
 const RootState = combineReducers(
     {
         todolists: todolistsReducer,
         tasks: tasksReducer,
-        app: appReducer
+        app: appReducer,
+        auth: authReducer
     }
 )
 
 export type RootStateType = ReturnType<typeof RootState>
-type AppActionTypes = TodolistsActionsType | TasksActionsType | AppActionsType
+type AppActionTypes = TodolistsActionsType | TasksActionsType | AppActionsType | AuthActionsType
 export type AppDispatchType = ThunkDispatch<RootStateType, unknown, AppActionTypes>
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootStateType, unknown, AppActionTypes>
 
