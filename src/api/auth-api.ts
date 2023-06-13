84a5a27ac1afc066ee7311142202cf3373f04c3d
vladnen97 <darkdescent1991@gmail.com>
2023-06-13 22:56:10 +0300

    auth api

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -1,4 +1,20 @@
+import {instance, ResponseType} from './index';
 
-const authApi = {
+export interface LoginParamsType {
+    email: string
+    password: string
+    rememberMe: boolean
+}
 
+
+export const authAPI = {
+    me() {
+        return instance.get<ResponseType<{id: number; email: string; login: string}>>('/auth/me')
+    },
+    login(params: LoginParamsType) {
+        return instance.post<ResponseType<{userId: number}>>('/auth/login', params)
+    },
+    logout() {
+        return instance.delete<ResponseType>('/auth/login')
+    }
 }
