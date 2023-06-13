7b6f6ee9857a5466ee698e2a6291b644582c0e76
vladnen97 <darkdescent1991@gmail.com>
2023-06-13 22:58:16 +0300

    wrapp with browser router

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import {createRoot} from 'react-dom/client';
 import {App} from './App';
 import {Provider} from 'react-redux';
 import {store} from './store/store';
+import {BrowserRouter} from 'react-router-dom';
 
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
 root.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>
 );
 
 
