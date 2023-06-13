41db48a261f9ddb361f02932aacaffd46f6954f1
vladnen97 <darkdescent1991@gmail.com>
2023-06-13 22:56:23 +0300

    auth reducer

--- src/store/auth-reducer.ts
+++ src/store/auth-reducer.ts
@@ -1,19 +1,59 @@
+import {setAppStatus} from './app-reducer';
+import {AppThunk} from './store';
+import {authAPI, LoginParamsType} from '../api/auth-api';
+import {handleServerAppError, handleServerNetworkError} from '../utils/error-utils';
+
 type InitStateType = typeof initState
-type AuthActionsType = any
+export type AuthActionsType = ReturnType<typeof setIsLoggedInAC>
 
-const initState = {}
+const initState = {
+    isLoggedIn: false
+}
 
-export const authReducer = (state:InitStateType = initState, action: AuthActionsType): InitStateType => {
+export const authReducer = (state: InitStateType = initState, action: AuthActionsType): InitStateType => {
     switch (action.type) {
-
+        case 'login/SET-IS-LOGGED-IN':
+            return {...state, isLoggedIn: action.value}
         default:
             return state
     }
 }
 
 
-//actions
-
-
+// actions
+export const setIsLoggedInAC = (value: boolean) =>
+    ({type: 'login/SET-IS-LOGGED-IN', value} as const)
 
 //thunks
+export const loginTC = (params: LoginParamsType): AppThunk => async dispatch => {
+    try {
+        dispatch(setAppStatus('loading'))
+
+        const res = await authAPI.login(params)
+
+        if (res.data.resultCode === 0) {
+            dispatch(setIsLoggedInAC(true))
+            dispatch(setAppStatus('succeeded'))
+        } else {
+            handleServerAppError(dispatch, res.data)
+        }
+    } catch (e) {
+        handleServerNetworkError(dispatch, e)
+    }
+}
+
+export const logoutTC = (): AppThunk => dispatch => {
+    dispatch(setAppStatus('loading'))
+    authAPI.logout()
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                dispatch(setIsLoggedInAC(false))
+                dispatch(setAppStatus('succeeded'))
+            } else {
+                handleServerAppError(dispatch, res.data)
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(dispatch, error)
+        })
+}
