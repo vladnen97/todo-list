75663364fc64943ab67ac1668b6f73ac026bc915
vladnen97 <darkdescent1991@gmail.com>
2023-06-13 22:58:46 +0300

    initialize state value + thunk

--- src/store/app-reducer.ts
+++ src/store/app-reducer.ts
@@ -1,11 +1,20 @@
+import {authAPI} from '../api/auth-api';
+import {AppThunk} from './store';
+import {setIsLoggedInAC} from './auth-reducer';
+import {handleServerAppError, handleServerNetworkError} from '../utils/error-utils';
+
 type InitStateType = typeof initState
 export type StatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
-export type AppActionsType = ReturnType<typeof setAppStatus> | ReturnType<typeof setAppError>
+export type AppActionsType =
+    ReturnType<typeof setAppStatus>
+    | ReturnType<typeof setAppError>
+    | ReturnType<typeof setIsInitialized>
 
 
 const initState = {
     status: 'idle' as StatusType,
-    error: null as string | null
+    error: null as string | null,
+    isInitialized: false
 }
 
 
@@ -16,15 +25,30 @@ export const appReducer = (state: InitStateType = initState, action: AppActionsT
             return {...state, status: action.status}
         case 'APP/SET-ERROR':
             return {...state, error: action.error}
+        case 'APP/SET-INITIALIZED':
+            return {...state, isInitialized: action.value}
 
         default:
             return state
     }
 }
 
-//action creators
+//action
 export const setAppStatus = (status: StatusType) => ({type: 'APP/SET-STATUS', status} as const)
 export const setAppError = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
-//thunk creators
+export const setIsInitialized = (value: boolean) => ({type: 'APP/SET-INITIALIZED', value} as const)
 
+//thunk
 
+export const initializeAppTC = (): AppThunk => dispatch => {
+    authAPI.me().then(res => {
+        if (res.data.resultCode === 0) {
+            dispatch(setIsLoggedInAC(true));
+            dispatch(setIsInitialized(true))
+        } else {
+            handleServerAppError(dispatch, res.data)
+        }
+    }).catch(e => {
+        handleServerNetworkError(dispatch, e)
+    })
+}
