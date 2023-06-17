9637b656eeb61428e825e4a0dd1950e20ad114ad
vladnen97 <darkdescent1991@gmail.com>
2023-06-17 16:00:26 +0300

    refactoring

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -2,12 +2,12 @@ import {setAppError, setAppStatus} from '../store/app-reducer';
 import {AppDispatchType} from '../store/store';
 import {ResponseType} from '../api';
 
-export const handleServerAppError = <D>(dispatch: AppDispatchType, res: ResponseType<D>) => {
+export const handleServerAppError = <D>(dispatch: AppDispatchType, res: ResponseType<D>): void => {
     dispatch(setAppError(res.messages[0]))
     dispatch(setAppStatus('failed'))
 }
 
-export const handleServerNetworkError = (dispatch: AppDispatchType, err: any) => {
+export const handleServerNetworkError = (dispatch: AppDispatchType, err: any): void => {
     dispatch(setAppError(err.response?.data?.message || err.message || 'Some error occurred'))
     dispatch(setAppStatus('failed'))
 }
