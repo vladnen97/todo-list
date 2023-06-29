3c923bd168add9a7baed1e1ab9e52748bcf846e6
vladnen97 <darkdescent1991@gmail.com>
2023-06-29 23:55:42 +0300

    typed error

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,13 +1,19 @@
 import { AppDispatchType } from "../store/store";
 import { ResponseType } from "../api";
 import {appActions} from '../store/app-reducer';
+import axios, {AxiosError} from 'axios';
 
 export const handleServerAppError = <D>(dispatch: AppDispatchType, res: ResponseType<D>): void => {
     dispatch(appActions.setAppError({error: res.messages[0]}));
     dispatch(appActions.setAppStatus({status: "failed"}));
 };
 
-export const handleServerNetworkError = (dispatch: AppDispatchType, err: any): void => {
-    dispatch(appActions.setAppError({error: err.response?.data.message || err.message || "Some error occurred"}));
+export const handleServerNetworkError = (dispatch: AppDispatchType, e: unknown): void => {
+    const err = e as Error | AxiosError<{ error: string }>
+    if (axios.isAxiosError(err)) {
+        dispatch(appActions.setAppError({error: err.response?.data.message || err.message || "Some error occurred"}));
+    } else {
+        dispatch(appActions.setAppError({error: `Native error ${err.message}`}));
+    }
     dispatch(appActions.setAppStatus({status: "failed"}));
 };
