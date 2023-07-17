fca34094bb6e23dc251c5d55f68c0a4659e5e7fc
vladnen97 <darkdescent1991@gmail.com>
2023-07-17 21:44:46 +0300

    fix: add error handling

--- src/common/utils/thunk-try-catch.ts
+++ src/common/utils/thunk-try-catch.ts
@@ -1,16 +1,16 @@
 import { BaseThunkAPI } from '@reduxjs/toolkit/dist/createAsyncThunk'
-import {handleServerNetworkError} from './handle-server-network-error';
 import {AppDispatchType, RootStateType} from '../../app/store';
+import {AxiosError} from 'axios';
 
 export const thunkTryCatch = async (
     thunkAPI: BaseThunkAPI<RootStateType, any, AppDispatchType, unknown>,
     logic: Function) => {
-    const {dispatch, rejectWithValue} = thunkAPI
+    const {rejectWithValue} = thunkAPI
 
     try {
         return await logic()
     } catch (e) {
-        handleServerNetworkError(dispatch, e)
-        return rejectWithValue(null)
+        const err = e as Error | AxiosError<{ error: string }>
+        return rejectWithValue({data: err.message})
     }
 }
